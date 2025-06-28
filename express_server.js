ab0e4fc7371ebb83af76fe14804e8eeeaadf000a
Pablo Almonacid <almonacid.pablo@hotmail.com>
2025-06-28 13:26:34 -0400

    refactor code

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
-const {generateRandomString, createUser, findUserByEmail} = require("./utils");
-const cookieParser = require("cookie-parser")
-const users = require("./data")
+const PORT = 8080;
+const {
+  generateRandomString,
+  createUser,
+  findUserByEmail,
+  authenticateUser
+} = require("./utils");
+const cookieParser = require("cookie-parser");
+const users = require("./data");
 
 
 const urlDatabase = {
@@ -11,87 +16,153 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-
+//set view engine to ejs
 app.set("view engine", "ejs");
-
 
 /////////////////////// MIDDLEWARE ///////////////////////////
 
-//middleware parse form data 
+//parse form data
 app.use(express.urlencoded({ extended: true }));
 
-//middleware to parse cookies
+//parse cookies
 app.use(cookieParser());
 
 /////////////////////// GET ROUTES ///////////////////////////
+
+
+app.get("/error", (req, res) => {
+  res.render("error");
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  const userId = req.cookies['user_id']; 
+  const userId = req.cookies['user_id'];
 
   const templateVars = { urls: urlDatabase, user: users[userId]};
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
-  const userId = req.cookies['user_id']; 
+  const userId = req.cookies['user_id'];
   const templateVars = {user: users[userId]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const userId = req.cookies['user_id']; 
+  const userId = req.cookies['user_id'];
 
-  const templateVars = { 
-    id: req.params.id, 
+  const templateVars = {
+    id: req.params.id,
     longURL: urlDatabase[req.params.id],
     user: users[userId]
   };
 
   res.render("urls_show", templateVars);
-})
+});
 
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
 
-  if(!longURL) res.status(404).send("<h1>404 - Not Found</h1>")
-  else res.status(301).redirect(longURL)
+  if (!longURL) {
+    const templateVars = {
+      errorCode: "404",
+      errorMessage: "Not found"
+    };
+    return res.status(404).render("error", templateVars);
+  }
+  
+  res.status(301).redirect(longURL);
 });
 
 app.get("/register", (req, res) => {
-  const userId = req.cookies['user_id']; 
+  const userId = req.cookies['user_id'];
   const templateVars = {user: users[userId]};
   res.render("register", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  const userId = req.cookies['user_id']; 
+  const userId = req.cookies['user_id'];
   const templateVars = {user: users[userId]};
   res.render("login", templateVars);
-})
+});
 
 /////////////////////// POST ROUTES ///////////////////////////
 
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //if credentials are valid, returns an authenticated user, otherwise returns null
+  const authUser = authenticateUser(email, password, users);
+
+  if (!authUser) {
+    const templateVars = {errorCode: "403", errorMessage: "Invalid Credentials"};
+    return res.status(403).render("error", templateVars);
+  }
+
+  res.status(200).cookie("user_id", authUser.id).redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  //remove cookie with user id
+  res.status(200).clearCookie("user_id").redirect("/login");
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+
+  //checks if email or password fields are empty when user submits form
+  if (email === "" || password === "") {
+    const templateVars = {
+      errorCode: "400",
+      errorMessage: "No email or password was entered"
+    };
+    return res.status(400).render("error", templateVars);
+  }
+  
+  const foundUser = findUserByEmail(email, users);
+
+  //checks if user with the email proivided already exists
+  if (foundUser) {
+    const templateVars = {
+      errorCode: "400",
+      errorMessage: "Email provided is already registered"
+    };
+    return res.status(400).render("error", templateVars);
+  }
+
+  const newUser = createUser({email, password}, users);
+
+  //sets new cookie with the newly created user's id
+  res.status(201).cookie("user_id", newUser.id).redirect("/urls");
+});
+
 
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL
+  const longURL = req.body.longURL;
   const id = generateRandomString(6);
 
   urlDatabase[id] = longURL;
-  res.status(201).redirect(`/urls/${id}`); 
+  res.status(201).redirect(`/urls/${id}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
 
-  if(!urlDatabase[id]) {
-    res.status(404).send("<h1>Something went wrong! could not delete URL</h1>")
-  } 
-  else {
+  if (!urlDatabase[id]) {
+    const templateVars = {
+      errorCode: "404",
+      errorMessage: "Something went wrong! could not delete URL"
+    };
+    res.status(404).render("error", templateVars);
+  } else {
     delete urlDatabase[id];
     res.status(200).redirect('/urls');
   }
@@ -99,48 +170,20 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id/update", (req, res) => {
   const id = req.params.id;
-  const updatedURL = req.body.updatedURL
+  const updatedURL = req.body.updatedURL;
 
-  if(!urlDatabase[id]) {
-    res.status(404).send("<h1>Something went wrong! could not update URL</h1>")
-  } 
-  else {
+  if (!urlDatabase[id]) {
+    const templateVars = {
+      errorCode: "404",
+      errorMessage: "Something went wrong! could not update URL"
+    };
+    res.status(404).render("error", templateVars);
+  } else {
     urlDatabase[id] = updatedURL;
     res.status(200).redirect('/urls');
   }
 });
 
-// app.post("/login", (req, res) => {
-//   const email = req.body.email;
-//   res.cookie("username", username).redirect("/urls");
-// });
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id").status(200).redirect("/urls"); 
-});
-
-app.post("/register", (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
-
-  //checks if email or password fields are empty when user submits form
-  if(email === "" || password === "") {
-    return res.status(400).send("<h1>400 - No email or password was entered.</h1>")
-  }
-  
-  //checks if user with the email proivided already exists 
-  if(findUserByEmail(email, users)) {
-    return res.status(400).send("<h1>400 - Email provided is already registered</h1>")
-  }
-
-  const newUser = createUser({email, password}, users);
-
-  //sets new cookie with the newly created user's id 
-  res.cookie("user_id", newUser.id) 
-
-  //status code 201 - successfully created new user
-  res.status(201).redirect("/urls");
-})
 
 //////////////////// START SERVER ///////////////////////////
 
--- utils.js
+++ utils.js
@@ -4,34 +4,48 @@ const generateRandomString = (length) => {
   
   // Loop to generate a random string of the given length
   for (let i = 0; i < length; i++) {
-      const random = Math.floor(Math.random() * characters.length);
-      result += characters.charAt(random);
+    const random = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(random);
   }
   return result;
-}
+};
 
 const createUser = (newUser, userDatabase) => {
-  //generate random id 
+  //generate random id
   const id = generateRandomString(5);
   
   //checks if user with that id already exists
   // if it does, runs the function again (recursion)
-  if(userDatabase[id]) return createUser(newUser, userDatabase);
+  if (userDatabase[id]) return createUser(newUser, userDatabase);
 
-  //set id property to newUser object 
+  //set id property to newUser object
   newUser["id"] = id;
 
   //add new user to user database
-  userDatabase[id] = newUser
+  userDatabase[id] = newUser;
 
   return newUser;
-}
+};
 
-const findUserByEmail = (email, users) => {
-  for(const key in users) {
-    if(users[key].email === email) return users[key];
+const findUserByEmail = (email, usersDatabase) => {
+  for (const key in usersDatabase) {
+    if (usersDatabase[key].email === email) return usersDatabase[key];
   }
   return null;
-}
+};
 
-module.exports = {generateRandomString, createUser, findUserByEmail};
+const authenticateUser = (email, password, usersDatabse) => {
+  for (const key in usersDatabse) {
+    if (usersDatabse[key].email === email && usersDatabse[key].password === password) {
+      return usersDatabse[key];
+    }
+  }
+  return null;
+};
+
+module.exports = {
+  generateRandomString,
+  createUser,
+  findUserByEmail,
+  authenticateUser
+};
